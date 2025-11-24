c39184cc71855f4778ef920859445ea95fb916c0
annabellening1 <annabellening1@gmail.com>
2025-11-24 12:56:13 -0500

    Plots.R

--- Plots.R
+++ Plots.R
@@ -1,20 +1,23 @@
-***Make sure you have the adjusted_pMatrices and the happyData before running code**
+##Plots
 
-PLOTS
 
 #making heatmap and adding color palette for mean GDP
 heatmap(adjusted_pMatrices[["meanGDP"]], 
-        Rowv = NA, Colv= NA,
-        col = colorRampPalette(c("darkgreen", "lightgreen", "white", 
-                                 "skyblue", "navy"))(200))
+  Rowv = NA, Colv= NA,
+  cexRow=0.5,
+  cexCol=0.5,
+  col = colorRampPalette(c("darkgreen", "lightgreen", "white", 
+                           "skyblue", "navy"))(200))
+#Adding a title
+title("Heatmap of meanGDP adjusted P-values")
 
 #Adding a legend on the side
-zlim <- range(adjusted_pMatrices[["meanGDP"]], na.rm = TRUE)
+zlim_meanGDP <- range(adjusted_pMatrices[["meanGDP"]], na.rm = TRUE)
 cols <- colorRampPalette(c("darkgreen", "lightgreen", "white", 
                            "skyblue", "navy"))(200)
 legend(
   "topleft", 
-  legend = round(seq(zlim[1], zlim[2], length.out = 10), 2),
+  legend = round(seq(zlim_meanGDP[1], zlim_meanGDP[2], length.out = 10), 2),
   fill = cols[seq(1, length(cols), length.out = 10)],                            
   border = NA,
   bty = "n",
@@ -23,24 +26,30 @@ legend(
 
 
 
-#for Mean Life Expectancy
+
+
+#for Mean Life expectancy
 heatmap(adjusted_pMatrices[["meanLifeExpectancy"]], 
         Rowv = NA, Colv= NA,
+        cexRow = 0.5,
+        cexCol = 0.5,
         col = colorRampPalette(c("darkgreen", "lightgreen", "white", 
                                  "skyblue", "navy"))(200))
+#Adding a title
+title("Heatmap of meanLifeExpectancy adjusted P-values")
 
 #Adding a legend on the side
-zlim <- range(adjusted_pMatrices[["meanGDP"]], na.rm = TRUE)
-cols <- colorRampPalette(c("darkgreen", "lightgreen", "white", 
-                           "skyblue", "navy"))(200)
+zlim_meanLifeExpectancy <- range(adjusted_pMatrices[["meanLifeExpectancy"]], na.rm = TRUE)
+
 legend(
   "topleft", 
-  legend = round(seq(zlim[1], zlim[2], length.out = 10), 2),
+  legend = round(seq(zlim_meanLifeExpectancy[1], zlim_meanLifeExpectancy[2], length.out = 10), 2),
   fill = cols[seq(1, length(cols), length.out = 10)],                            
   border = NA,
   bty = "n",
   title = "Value"
 )
+
 
 
 
@@ -48,16 +57,19 @@ legend(
 #for mean Freedom
 heatmap(adjusted_pMatrices[["meanFreedom"]], 
         Rowv = NA, Colv= NA,
+        cexRow = 0.5,
+        cexCol = 0.5,
         col = colorRampPalette(c("darkgreen", "lightgreen", "white", 
                                  "skyblue", "navy"))(200))
+#Adding a title
+title("Heatmap of meanFreedom adjusted P-values")
 
 #Adding a legend on the side
-zlim <- range(adjusted_pMatrices[["meanGDP"]], na.rm = TRUE)
-cols <- colorRampPalette(c("darkgreen", "lightgreen", "white", 
-                           "skyblue", "navy"))(200)
+zlim_meanFreedom <- range(adjusted_pMatrices[["meanFreedom"]], na.rm = TRUE)
+
 legend(
   "topleft", 
-  legend = round(seq(zlim[1], zlim[2], length.out = 10), 2),
+  legend = round(seq(zlim_meanFreedom[1], zlim_meanFreedom[2], length.out = 10), 2),
   fill = cols[seq(1, length(cols), length.out = 10)],                            
   border = NA,
   bty = "n",
@@ -70,8 +82,8 @@ legend(
 
 #Making a plot of points and cut off line
 
-
-##making for mean GDP
+#for mean GDP
+# Suppose your matrix of adjusted p-values is:
 adj_p_meanGDP <- adjusted_pMatrices[["meanGDP"]]
 
 # Flatten to a vector
@@ -83,7 +95,6 @@ adj_p_vec_meanGDP <- adj_p_vec_meanGDP[!is.na(adj_p_vec_meanGDP)]
 #sort the values
 sorted_adj_p_meanGDP <- sort(adj_p_vec_meanGDP)
 
-#plot the points
 plot(
   1:length(sorted_adj_p_meanGDP),
   sorted_adj_p_meanGDP,
@@ -103,6 +114,7 @@ abline(h = 0.05, col = "blue", lty = 2)
 
 
 #for mean LIFE EXPECTANCY
+# Suppose your matrix of adjusted p-values is:
 adj_p_meanLifeExpectancy <- adjusted_pMatrices[["meanLifeExpectancy"]]
 
 # Flatten to a vector
@@ -114,7 +126,6 @@ adj_p_vec_meanLifeExpectancy <- adj_p_vec_meanLifeExpectancy[!is.na(adj_p_vec_me
 #sort the values
 sorted_adj_p_meanLifeExpectancy <- sort(adj_p_vec_meanLifeExpectancy)
 
-#plot the points
 plot(
   1:length(sorted_adj_p_meanLifeExpectancy),
   sorted_adj_p_meanLifeExpectancy,
@@ -134,6 +145,7 @@ abline(h = 0.05, col = "blue", lty = 2)
 
 
 ##for mean FREEDOM
+# Suppose your matrix of adjusted p-values is:
 adj_p_meanFreedom <- adjusted_pMatrices[["meanFreedom"]]
 
 # Flatten to a vector
@@ -145,7 +157,6 @@ adj_p_vec_meanFreedom <- adj_p_vec_meanFreedom[!is.na(adj_p_vec_meanFreedom)]
 #sort the values
 sorted_adj_p_meanFreedom <- sort(adj_p_vec_meanFreedom)
 
-#plot the points
 plot(
   1:length(sorted_adj_p_meanFreedom),
   sorted_adj_p_meanFreedom,
@@ -164,6 +175,7 @@ abline(h = 0.05, col = "blue", lty = 2)
 
 
 
+
 ##Barcharts of the means by region
 
 #for meanGDP
@@ -173,8 +185,11 @@ barplot(
   col = "steelblue",
   ylab = "Mean GDP Score",
   main = "Mean GDP Score by Region",
-  las = 2  # rotate x-axis labels if needed
+  las = 2,  # rotate region names 90 degrees
+  cex.names = 0.3 #make region names smaller
 )
+
+
 
 
 
@@ -185,8 +200,10 @@ barplot(
   col = "steelblue",
   ylab = "Mean Life Expectancy Score",
   main = "Mean Life Expectancy Score by Region",
-  las = 2  # rotate x-axis labels if needed
+  las = 2, #rotate region names 90 degrees
+  cex.names = 0.3 #make region names smaller
 )
+
 
 
 
@@ -197,5 +214,6 @@ barplot(
   col = "steelblue",
   ylab = "Mean Freedom Score",
   main = "Mean Freedom Score by Region",
-  las = 2  # rotate x-axis labels if needed
+  las = 2,  # rotate region names 90 degrees
+  cex.names = 0.3 #make region names smaller
 )
